91b43312f89b9034dbc8880d6ca379edc86fe998
xuzhen <2554463433@qq.com>
2018-11-17 11:28:10 +0800

    路由改为懒加载

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Register from '@/components/Register'
-import Login from '@/components/Login'
+// import Register from '@/components/Register'
+// import Login from '@/components/Login'
+// 路由懒加载
+const Login = resovle => {
+  require.ensure(['@/components/Login.vue'], () => {
+    resovle(require('@/components/Login.vue'))
+  })
+}
+const Register = resovle => {
+  require.ensure(['@/components/Register.vue'], () => {
+    resovle(require('@/components/Register.vue'))
+  })
+}
 Vue.use(Router)
 
 export default new Router({
